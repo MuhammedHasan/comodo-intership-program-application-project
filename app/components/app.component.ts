26be706403bd14e4db10a19aac261cc2993f92bb
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-03-24 21:46:41 +0200

    sign up

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup';
 import 'rxjs/Rx'
 
 
@@ -13,6 +14,6 @@ import 'rxjs/Rx'
 @RouteConfig([
   { path: '/', name: 'Home', component: HomeComponent, useAsDefault: true },
   { path: '/login', name: 'Login', component: LoginComponent, },
-
+  { path: '/signup', name: 'Signup', component: SignupComponent, },
 ])
 export class AppComponent { }
