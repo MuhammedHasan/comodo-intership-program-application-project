2a3477ec186f74ef4baff770bcb7cf514fb4e6ed
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-03-24 17:50:15 +0200

    main page

--- app/boot.ts
+++ app/boot.ts
@@ -2,8 +2,7 @@ import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './components/app.component'
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
-import {DbService} from './services/db.service';
 
 bootstrap(AppComponent, [
-  ROUTER_PROVIDERS, HTTP_PROVIDERS, DbService
+  ROUTER_PROVIDERS, HTTP_PROVIDERS
 ]);
